0d2fdded5bfbc0a9c96d6f2bd486bbcd38f791c7
shuix007 <shuix007@umn.edu>
2024-03-03 16:54:19 -0600

    Debug.

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,7 +3,8 @@
 #SBATCH -p gk
 #SBATCH --nodes=1                    # Number of nodes
 #SBATCH --cpus-per-task=5
-#SBATCH --ntasks-per-node=1          # How many tasks on each node
+#SBATCH --ntasks-per-node=4          # How many tasks on each node
+#SBATCH --gpus-per-task=1
 #SBATCH --gres=gpu:4                 # Number of GPUs per node
 #SBATCH --time=01:00:00              # Time limit hrs:min:sec
 #SBATCH --output=ddp_%j.log          # Standard output and error log
