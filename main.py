3b580bcc4bc873640334398527d817b627fea717
shuix007 <shuix007@umn.edu>
2024-03-05 17:43:11 -0600

    Exp.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import numpy as np
 from pymatgen.core import Structure
 from chgnet.trainer import Trainer
 from chgnet.model import CHGNet
-from chgnet.data.dataset import StructureData, get_train_val_test_loader
+from chgnet.data.dataset import StructureData
 from chgnet.utils import distutils
 
 def set_seed(seed):
@@ -19,49 +19,6 @@ def set_seed(seed):
     torch.cuda.manual_seed_all(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
-
-# def load_structures_from_json(
-#         json_filename='../FERMat/Data/MPtrj_2022.9_full.json',
-#         sample_filename='../FERMat/Data/SampledPretrain/MPtraj-semdedup/rseed0-0.10/colabfit_ids.txt'
-#     ):
-#     with open(json_filename, 'r') as f:
-#         data = json.load(f)
-    
-#     if sample_filename is not None:
-#         with open(sample_filename, 'r') as f:
-#             sample_ids = set(f.read().splitlines())
-    
-#     structures = []
-#     energies_per_atom = []
-#     forces = []
-#     stresses = []
-#     magmoms = []
-
-#     mp_ids = sorted(list(data.keys()))
-#     for mp_id in mp_ids:
-#         frame_ids = sorted(list(data[mp_id].keys())) # deterministic order
-#         for frame_id in frame_ids:
-#             if sample_filename is not None and frame_id not in sample_ids:
-#                 continue
-#             frame = data[mp_id][frame_id]
-            
-#             structures.append(Structure.from_dict(frame['structure']))
-#             energies_per_atom.append(frame['energy_per_atom'])
-#             forces.append(frame['force'])
-#             stresses.append(frame['stress'])
-#             magmoms.append(frame['magmom'])
-
-#     dataset = StructureData(
-#         structures=structures,
-#         energies=energies_per_atom,
-#         forces=forces,
-#         stresses=stresses,  # can be None
-#         magmoms=magmoms,  # can be None
-#     )
-#     train_loader, val_loader, test_loader = get_train_val_test_loader(
-#         dataset, batch_size=40, train_ratio=0.9, val_ratio=0.05
-#     )
-#     return train_loader, val_loader, test_loader
 
 def load_structures_from_json(
         json_filename='../FERMat/Data/MPtrj_2022.9_full.json',
@@ -161,11 +118,11 @@ def main(args):
         criterion='Huber',
         delta=0.1,
         local_rank=local_rank,
-        epochs=2,
+        epochs=40,
         starting_epoch=0,
         learning_rate=5e-3,
         use_device='cuda',
-        print_freq=10
+        print_freq=500
     )
     trainer.load_datasets(
         dataset,
@@ -176,7 +133,7 @@ def main(args):
         seed=args.seed,
         num_workers=0
     )
-    trainer.train()
+    trainer.train(save_dir=args.identifier)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -207,6 +164,12 @@ if __name__ == '__main__':
         type=str,
         default="nccl",
         help="Backend for DDP",
+    )
+    parser.add_argument(
+        "--identifier",
+        type=str,
+        default=None,
+        help="Identifier for experiments.",
     )
     parser.add_argument(
         "--distributed", action="store_true", help="Run with DDP"
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,48 +1,26 @@
 #!/bin/bash
 #SBATCH --job-name=pytorch_ddp       # Job name
 #SBATCH -p gk
-#SBATCH --nodes=2                    # Number of nodes
+#SBATCH --nodes=6                    # Number of nodes
 #SBATCH --cpus-per-task=5
 #SBATCH --ntasks-per-node=1          # How many tasks on each node
 #SBATCH --gres=gpu:4                 # Number of GPUs per node
-#SBATCH --time=01:00:00              # Time limit hrs:min:sec
+#SBATCH --time=24:00:00              # Time limit hrs:min:sec
 #SBATCH --output=ddp_%j.log          # Standard output and error log
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=shuix007@umn.edu
 
-# Load any modules and activate your Python environment here
-
-# Clear any previously set PyTorch environment variables
 unset PYTHONPATH
 
 conda activate Torch2.1
 cd /home/karypisg/shuix007/FERMat/chgnet
 
 # Setup environment variables for distributed PyTorch
-# export WORLD_SIZE=$(($SLURM_NNODES * $SLURM_NTASKS_PER_NODE))
 export OMP_NUM_THREADS=8
 export TORCH_DISTRIBUTED_DEBUG=DETAIL
 
 export MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
 export MASTER_PORT=13356
-
-# Run the PyTorch script using torch.distributed.launch or torchrun (for PyTorch >= 1.9)
-# srun python -m torch.distributed.launch --nproc_per_node=$SLURM_NTASKS_PER_NODE --nnodes=$SLURM_NNODES --node_rank=$SLURM_PROCID --master_addr=$MASTER_ADDR --master_port=$MASTER_PORT your_training_script.py
-
-# echo $MASTER_ADDR:$MASTER_PORT
-# echo $SLURM_NTASKS_PER_NODE
-# echo $SLURM_NNODES
-# echo $WORLD_SIZE
-# echo $SLURM_JOB_ID
-
-# If using PyTorch 1.9 or newer, you can also use torchrun (which replaces torch.distributed.launch)
-# srun torchrun --nproc_per_node=$SLURM_NTASKS_PER_NODE \
-#             --nnodes=$SLURM_NNODES \
-#             --max-restarts=1 \
-#             --rdzv_id=$SLURM_JOB_ID \
-#             --rdzv_backend=c10d \
-#             --rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT \
-#             main.py --submit --distributed --num-nodes 2 --num-gpus 4
 
 srun torchrun --nproc_per_node=4 \
             --nnodes=$SLURM_NNODES \
@@ -50,5 +28,11 @@ srun torchrun --nproc_per_node=4 \
             --rdzv_id=$SLURM_JOB_ID \
             --rdzv_backend=c10d \
             --rdzv_endpoint=$MASTER_ADDR:$MASTER_PORT \
-            main.py --submit --distributed --num-nodes $SLURM_NNODES --num-gpus 4
+            main.py \
+            --submit \
+            --distributed \
+            --num-nodes $SLURM_NNODES \
+            --num-gpus 4 \
+            --json_filename data/MPtrj_2022.9_full.json \
+            --identifier MPtraj_full_CL 
             
